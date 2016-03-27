bad0283ceada8e011659d84dbae5d2d554ed8ecd
Justin Aquadro <jaquadro@gmail.com>
2016-03-27 15:26:13 -0400

    Parity capability with Chameleon 1.x

---
+++ src/com/jaquadro/minecraft/chameleon/model/ProxyBuilderModel.java
@@ -0,0 +1,91 @@
+package com.jaquadro.minecraft.chameleon.model;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.block.model.ItemOverrideList;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.EnumFacing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class ProxyBuilderModel implements IBakedModel
+{
+    private static final List<BakedQuad> EMPTY = new ArrayList<BakedQuad>(0);
+
+    private IBakedModel parent;
+    private IBakedModel proxy;
+    private IBlockState stateCache;
+    private TextureAtlasSprite iconParticle;
+
+    public ProxyBuilderModel (TextureAtlasSprite iconParticle) {
+        this.iconParticle = iconParticle;
+    }
+
+    public ProxyBuilderModel (IBakedModel parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public List<BakedQuad> getQuads (IBlockState state, EnumFacing side, long rand) {
+        if (proxy == null || stateCache != state)
+            setProxy(state);
+
+        if (proxy == null)
+            return EMPTY;
+
+        return proxy.getQuads(state, side, rand);
+    }
+
+    @Override
+    public boolean isAmbientOcclusion () {
+        IBakedModel model = getActiveModel();
+        return (model != null) ? model.isAmbientOcclusion() : true;
+    }
+
+    @Override
+    public boolean isGui3d () {
+        IBakedModel model = getActiveModel();
+        return (model != null) ? model.isGui3d() : false;
+    }
+
+    @Override
+    public boolean isBuiltInRenderer () {
+        IBakedModel model = getActiveModel();
+        return (model != null) ? model.isBuiltInRenderer() : false;
+    }
+
+    @Override
+    public TextureAtlasSprite getParticleTexture () {
+        IBakedModel model = getActiveModel();
+        return (model != null) ? model.getParticleTexture() : iconParticle;
+    }
+
+    @Override
+    public ItemCameraTransforms getItemCameraTransforms () {
+        IBakedModel model = getActiveModel();
+        return (model != null) ? model.getItemCameraTransforms() : ItemCameraTransforms.DEFAULT;
+    }
+
+    @Override
+    public ItemOverrideList getOverrides () {
+        IBakedModel model = getActiveModel();
+        return (model != null) ? model.getOverrides() : ItemOverrideList.NONE;
+    }
+
+    protected abstract IBakedModel buildModel (IBlockState state, IBakedModel parent);
+
+    private void setProxy (IBlockState state) {
+        stateCache = state;
+        if (state == null)
+            proxy = parent;
+        else
+            proxy = buildModel(state, parent);
+    }
+
+    private IBakedModel getActiveModel () {
+        return (proxy != null) ? proxy : parent;
+    }
+}
