dd515454966116bef47ad88727180772ca6f63cc
Justin Aquadro <jaquadro@gmail.com>
2017-07-05 19:59:48 -0400

    TileEntityShim implements INBTSerializable

--- src/com/jaquadro/minecraft/chameleon/block/tiledata/TileDataShim.java
+++ src/com/jaquadro/minecraft/chameleon/block/tiledata/TileDataShim.java
@@ -1,10 +1,22 @@
 package com.jaquadro.minecraft.chameleon.block.tiledata;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.INBTSerializable;
 
-public abstract class TileDataShim
+public abstract class TileDataShim implements INBTSerializable<NBTTagCompound>
 {
     public abstract void readFromNBT (NBTTagCompound tag);
 
     public abstract NBTTagCompound writeToNBT (NBTTagCompound tag);
+
+    @Override
+    public NBTTagCompound serializeNBT () {
+        NBTTagCompound tag = new NBTTagCompound();
+        return writeToNBT(tag);
+    }
+
+    @Override
+    public void deserializeNBT (NBTTagCompound nbt) {
+        readFromNBT(nbt);
+    }
 }
